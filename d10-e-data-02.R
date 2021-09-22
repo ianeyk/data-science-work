a559145bfdc2b237554a05e99f3b431dfe3363a1
Ian Eykamp <ian@eykamp.com>
2021-09-22 00:46:30 -0400

    add challenge exercises

--- d10-e-data-02.R
+++ d10-e-data-02.R
@@ -30,11 +30,7 @@ top_10_names <- babynames %>%
   summarize(total = sum(n)) %>% 
   arrange(desc(total)) %>% 
   ungroup() %>% 
-  slice(1:10) %>% 
-
-top_10 <- babynames %>% 
-  # filter((name == "Ian" & sex == "M") | (name == "Sam" & sex == "F"))
-View(top_10)
+  slice(1:10)
 
 # best solution
 top_10 <- babynames %>% 
@@ -48,5 +44,55 @@ babynames %>%
   filter(name == "James", sex == "M") %>% 
   summarize(n = sum(n))
 
+babynames %>% 
+  mutate(births = n / prop)
 
+babynames %>% 
+  group_by(year, sex) %>% 
+  mutate(rank = min_rank(desc(prop))) %>% 
+  group_by(name, sex) %>% 
+  summarize(median = median(rank)) %>% 
+  arrange(median)
+
+
+babynames %>% 
+  group_by(year, sex) %>% 
+  mutate(rank = min_rank(desc(prop))) %>% 
+  filter(rank == 1, sex == "M") %>% 
+  ungroup() %>% 
+  distinct(name) %>%
+  summarize(num = n())
+
+number_ones <- 
+  babynames %>% 
+  group_by(year, sex) %>% 
+  mutate(rank = min_rank(desc(prop))) %>% 
+  filter(rank == 1, sex == "M") %>% 
+  ungroup() %>% 
+  distinct(name) %>%
+  unlist() %>%
+  print()
+
+babynames %>% 
+  filter(name %in% number_ones, sex == "M") %>% 
+  ggplot() + 
+  geom_line(mapping = aes(x = year, y = prop, color = name))
+
+babynames %>% 
+  group_by(year, sex) %>% 
+  summarize(n_names = n()) %>% 
+  ggplot() + 
+  geom_line(mapping = aes(x = year, y = n_names, color = sex))
+
+babynames %>% 
+  group_by(year, sex) %>% 
+  summarize(total = sum(n)) %>% 
+  ggplot() + 
+  geom_line(mapping = aes(x = year, y = total, color = sex))
+
+babynames %>% 
+  group_by(year, sex) %>% 
+  summarize(children_per_name = sum(n) / n()) %>% 
+  ggplot() + 
+  geom_line(mapping = aes(x = year, y = children_per_name, color = sex))
 
